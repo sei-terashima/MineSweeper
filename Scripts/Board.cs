91ae8bbc3def6cef4a0862d3f932c74d7e358704
NEXTGEAR\terag <terashima.sei@gmail.com>
2025-01-31 00:34:29 +0900

    スクリプト更新

--- Scripts/Board.cs
+++ Scripts/Board.cs
@@ -25,34 +25,62 @@ public class Board : MonoBehaviour
         tilemap = GetComponent<Tilemap>();
     }
 
-    public void OnDraw(Cell[,] state)
+    public void Draw(Cell[,] state)
     {
         int width = state.GetLength(0);
         int height = state.GetLength(1);
 
-        for(int x = 0; x <width; x++)
+        for (int x = 0; x < width; x++)
         {
-            for(int y = 0; y <height; y++)
+            for (int y = 0; y < height; y++)
             {
-                Cell cell = state[x,y];
-                //tilemap.SetTile(cell.position, GetTile(cell));
+                Cell cell = state[x, y];
+                tilemap.SetTile(cell.position, GetTile(cell));
             }
         }
     }
 
-    //private Tile GetTile(Cell cell)
-    //{
-    //    if(cell.revealed) {
-    //        //...
-    //    }else if (cell.flagged) {
-    //        //...
-    //    }
-    //    else
-    //    {
-    //        //...
-    //    }
-    //}
+    private Tile GetTile(Cell cell)
+    {
+        if (cell.revealed)
+        {
+            return GetRevealedTile(cell);
+        }
+        else if (cell.flagged)
+        {
+            return tileFlag;
+        }
+        else
+        {
+            return tileUnknown;
+        }
+    }
 
+    private Tile GetRevealedTile(Cell cell)
+    {
+        switch(cell.type)
+        {
+            case Cell.Type.Empty: return tileEmpty;
+            case Cell.Type.Mine: return tileMine;
+            case Cell.Type.Number: return GetNumberTile(cell);
+            default: return null;
+        }
+    }
 
+    private Tile GetNumberTile(Cell cell)
+    {
+        switch (cell.number)
+        {
+            case 1: return tileNum1;
+            case 2: return tileNum2;
+            case 3: return tileNum3;
+            case 4: return tileNum4;
+            case 5: return tileNum5;
+            case 6: return tileNum6;
+            case 7: return tileNum7;
+            case 8: return tileNum8;
+            default: return null;
+        }
+    }
 
 }
